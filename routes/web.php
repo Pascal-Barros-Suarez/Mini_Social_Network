3b3beee9671151c4fd2e269af4c3c3dbdab7d8b7
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-01-25 20:19:43 +0000

    usado helpper env

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
 });
 
 Route::get('/dashboard', function () {
@@ -37,17 +36,17 @@ require __DIR__ . '/auth.php';
 
 //Crea una ruta que tenga un parámetro que sea opcional y comprueba que funciona.
 Route::get('/wellcome/{nombre?}', function ($nombre = null) {
-    return 'wellcome '.$nombre;
+    return 'wellcome ' . $nombre;
 });
 
 //Crea una ruta que tenga un parámetro que sea opcional y tenga un valor por defecto en caso de que no se especifique.
 Route::get('/goodbye/{nombre?}', function ($nombre = 'Pascal') {
-    return 'goodbye '.$nombre;
+    return 'goodbye ' . $nombre;
 });
 
 // Crea una ruta que atienda por POST y compruébala con Postman.
 Route::post('/wellcome/{nombre?}', function ($nombre = 'Pascal') {
-    return 'hi '.$nombre;
+    return 'hi ' . $nombre;
 });
 
 // Crea una ruta que atienda por GET y por POST (en un único método) y compruébalas. Vuelve a habilitar el filtro VerifyCsrfToken en el fichero kernel.php (carpeta Http).
@@ -57,10 +56,15 @@ Route::match(['get', 'post'], '/getpost', function () {
 
 //Crea una ruta que compruebe que un parámetro está formado sólo por números.
 Route::get('/num/{num}', function ($num) {
-    return 'tu numero: '.$num;
+    return 'tu numero: ' . $num;
 });
 
 //Crea una ruta con dos parámetros que compruebe que el primero está formado sólo por letras y el segundo sólo por números.
 Route::get('/numlet/{num}/{leter}', function ($num, $leter) {
-    return 'tu numero: '.$num. ' y tu frase: '.$leter;
+    return 'tu numero: ' . $num . ' y tu frase: ' . $leter;
+});
+
+//Utiliza el helper env para que cuando se acceda a la ruta /host nos devuleva la dirección IP donde se encuentra la base de datos de nuestro proyecto.
+Route::get('/host', function () {
+    return 'tu ip: ' . env('DB_HOST');
 });
