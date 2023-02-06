53f02e59dd688619bb74b28f62925fb8c029d5f9
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-02-06 00:48:24 +0000

    persistencia de datos

--- resources/views/layouts/add-link.blade.php
+++ resources/views/layouts/add-link.blade.php
@@ -9,8 +9,8 @@
                 <div class="form-group">
                     <label for="title">Title:</label>
 
-                    <input type="text" class="form-control @error('title') is-invalid @enderror"
-                        id="title" name="title" placeholder="What is the title of your article?">
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                        name="title" value="{{ old('title') }}" placeholder="What is the title of your article?">
 
                     @error('title')
                         <div class="alert alert-danger">{{ $message }}</div>
@@ -20,22 +20,26 @@
                 <div class="form-group">
                     <label for="Channel">Channel:</label>
                     <select class="form-control @error('channel_id') is-invalid @enderror" name="channel_id">
-                    <option selected disabled>Pick a Channel...</option>
-                    @foreach ($channels as $channel)
-                    <option value="{{ $channel->id }}">
-                    {{ $channel->title }}
-                    </option>
-                    @endforeach
+                        <option selected disabled>Pick a Channel...</option>
+                        @foreach ($channels as $channel)
+                            {{-- esta linea establece el valor del option y la establice como seleccionada si cumple la condicion --}}
+                            <option value="{{ $channel->id }}"
+                                {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+
+                                {{ $channel->title }}
+                            </option>
+                        @endforeach
                     </select>
                     @error('channel_id')
-                    <span class="text-danger">{{ $message }}</span>
+                        <span class="text-danger">{{ $message }}</span>
                     @enderror
-                    </div>
+                </div>
 
+                {{-- si intento enviar 2 veces el mismo link da un error en la clase store Integrity constraint violation: 1062 --}}
                 <div class="form-group">
                     <label for="link">Link:</label>
-                    <input type="text" class="form-control @error('link') is-invalid @enderror"
-                        id="link" name="link" placeholder="What is the URL?">
+                    <input type="text" class="form-control @error('link') is-invalid @enderror" id="link"
+                        name="link" value="{{ old('link') }}" placeholder="What is the URL?">
                     @error('link')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
