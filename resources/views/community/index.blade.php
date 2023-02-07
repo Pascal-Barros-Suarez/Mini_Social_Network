acf5530c3cbedfcfc700336e166ec29987f7adfb
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-02-07 19:57:09 +0000

    añadido de stilos css

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -10,14 +10,17 @@
                 @else
                     @foreach ($links as $link)
                         <li>
-                            <span class="label label-default" style="background: {{ $link->channel->color }}">
-                                {{ $link->channel->title }}
-                            </span>
-                            <a href="{{ $link->link }}" target="_blank">
-                                {{ $link->title }}
-                            </a>
-                            <small>Contributed by: {{ $link->creator->name }}
-                                {{ $link->updated_at->diffForHumans() }}</small>
+                            <div class="spaceInLi">
+                                <span class="text-span-in-li label label-default"
+                                    style="background: {{ $link->channel->color }}">
+                                    {{ $link->channel->title }}
+                                </span>
+                                <a href="{{ $link->link }}" target="_blank">
+                                    {{ $link->title }}
+                                </a>
+                                <small>Contributed by: {{ $link->creator->name }}
+                                    {{ $link->updated_at->diffForHumans() }}</small>
+                            </div>
                         </li>
                     @endforeach
                 @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
+    
     <!-- Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
+
+    @vite(['resources/css/app.css','resources/css/style.css', 'resources/js/app.js'])
+    
 </head>
 
 <body class="font-sans antialiased">
