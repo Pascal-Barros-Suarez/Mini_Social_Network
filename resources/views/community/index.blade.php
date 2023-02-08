554cf3b5eebb89a47607adccd881d7eaf2352973
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-02-08 19:43:09 +0000

    arreglo de stilos en el css

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -8,25 +8,27 @@
                 @if (count($links) == 0)
                     <h3>No contributions yet</h3>
                 @else
-                    @foreach ($links as $link)
-                        <li>
-                            <div class="spaceInLi">
-                                <span class="text-span-in-li label label-default"
-                                    style="background: {{ $link->channel->color }}">
-                                    {{ $link->channel->title }}
-                                </span>
-                                <a href="{{ $link->link }}" target="_blank">
-                                    {{ $link->title }}
-                                </a>
-                                <small>Contributed by: {{ $link->creator->name }}
-                                    {{ $link->updated_at->diffForHumans() }}</small>
-                            </div>
-                        </li>
-                    @endforeach
+                    <ul>
+                        @foreach ($links as $link)
+                            <li>
+                                <div class="spaceInLi">
+                                    <span class="text-span-in-li label label-default"
+                                        style="background: {{ $link->channel->color }}">
+                                        {{ $link->channel->title }}
+                                    </span>
+                                    <a href="{{ $link->link }}" target="_blank">
+                                        {{ $link->title }}
+                                    </a>
+                                    <small>Contributed by: {{ $link->creator->name }}
+                                        {{ $link->updated_at->diffForHumans() }}</small>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
                 @endif
             </div>
             {{-- @include('../layouts/add-link'); --}}
-            @include('layouts.add-link');
+            @include('layouts.add-link')
 
         </div>
         {{ $links->links() }}
